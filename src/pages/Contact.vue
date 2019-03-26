39df0ab4e0c4958d8bf46c1704cc9d7b49af28a4
Dan Shields <danwshields@gmail.com>
2019-03-25 20:36:44 -0600

    card contact

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,7 +1,15 @@
 <template>
   <Layout>
-    <h1>Contact Us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <div class="bg-card m-4 mx-auto max-w-sm shadow-lg rounded-lg">
+      <div class="p-4 text-center sm:flex-grow">
+        <div class="m-4">
+          <h1 class="text-xl">Contact CORE Assembly</h1>
+          <p class="pt-4 text-xm leading-tight text-grey-darker">
+            All the deets
+          </p>
+        </div>
+      </div>
+    </div>
   </Layout>
 </template>
 
