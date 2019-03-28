4eaebf6b9f79c4d85208069dfb745b196e2d1702
Dan Shields <danwshields@gmail.com>
2019-03-27 18:08:26 -0600

    fix merge conflict error

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -5,11 +5,7 @@
         <div class="m-4">
           <h1 class="text-xl">Contact CORE Assembly</h1>
           <p class="pt-4 text-xm leading-tight text-grey-darker">
-<<<<<<< HEAD
-            Email us at <a href="mailto:hello@coreassemlby.com">hello@coreassemlby.com</a>
-=======
             Email us at <a href="maillto:hello@coreassemlby.com">hello@coreassemlby.com</a>
->>>>>>> 0cc94b7b871d20d7027e70ab912b84b3a63cc02a
           </p>
         </div>
       </div>
